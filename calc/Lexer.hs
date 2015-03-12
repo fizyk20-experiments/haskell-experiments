6cdbec240712e2174c2d1b7e2fd91e8e4fd2d4ed
Bartłomiej Kamiński <fizyk20@gmail.com>
2015-03-12 16:47:59 +0100

    Added hexadecimal numbers support

--- calc/Lexer.hs
+++ calc/Lexer.hs
@@ -2,6 +2,7 @@ module Lexer where
 
 import Control.Monad
 import Control.Monad.State
+import Data.Char
 
 -- possible tokens
 data Token = Number Double | Operator Char | LeftParen | RightParen deriving (Show)
@@ -20,10 +21,25 @@ data LexerState = LexerError String
 				| LexerHexNumber { pos :: Int, token :: String, remains :: String }
 				| LexerFinished
 
+ord' :: (Num a) => Char -> a
+ord' = fromIntegral . ord
+
+readHex' :: Double -> String -> Double
+readHex' x [] = x
+readHex' x (c:rest)
+	| c `elem` digits = readHex' (x*16 + ord' c - 48) rest
+	| c `elem` hexDigitsSmall = readHex' (x*16 + ord' c - 87) rest
+	| c `elem` hexDigitsCapital = readHex' (x*16 + ord' c - 55) rest
+	| otherwise = 0.0
+
+readHex :: String -> Double
+readHex = readHex' 0
+
 consumeToken :: State LexerState (Maybe Token)
 consumeToken = state $ \s -> case s of
 	LexerError err -> (Nothing, LexerError err)
 	LexerFinished -> (Nothing, LexerFinished)
+
 	LexerReady _ "" -> (Nothing, LexerFinished)
 	LexerReady i (c:rest)
 		| c `elem` digits -> runState consumeToken $ LexerNumber (i+1) [c] rest
@@ -31,16 +47,26 @@ consumeToken = state $ \s -> case s of
 		| c == '(' -> (Just LeftParen, LexerReady (i+1) rest)
 		| c == ')' -> (Just RightParen, LexerReady (i+1) rest)
 		| otherwise -> lexerError i c
+
 	LexerNumber i t [] -> (Just $ Number $ read t, LexerFinished)
 	LexerNumber i t (c:rest)
 		| c `elem` digits -> runState consumeToken $ LexerNumber (i+1) (t ++ [c]) rest
+		| c `elem` (hexDigitsSmall ++ hexDigitsCapital) -> runState consumeToken $ LexerHexNumber (i+1) (t ++ [c]) rest
 		| c == '.' -> runState consumeToken $ LexerNumberDot (i+1) (t ++ [c]) rest
+		| c `elem` hexSuffixes -> (Just $ Number $ readHex t, LexerReady (i+1) rest)
 		| otherwise -> (Just $ Number $ read t, LexerReady i (c:rest))
+
 	LexerNumberDot i t [] -> (Just $ Number $ read t, LexerFinished)
 	LexerNumberDot i t (c:rest)
 		| c `elem` digits -> runState consumeToken $ LexerNumberDot (i+1) (t ++ [c]) rest
 		| c == '.' -> lexerError i c
 		| otherwise -> (Just $ Number $ read t, LexerReady i (c:rest))
+
+	LexerHexNumber i t [] -> (Nothing, LexerError "Unexpected end of input: unfinished hexadecimal number")
+	LexerHexNumber i t (c:rest)
+		| c `elem` (digits ++ hexDigitsCapital ++ hexDigitsSmall) -> runState consumeToken $ LexerHexNumber (i+1) (t ++ [c]) rest
+		| c `elem` hexSuffixes -> (Just $ Number $ readHex t, LexerReady (i+1) rest)
+		| otherwise -> lexerError i c
 
 	where lexerError i c = (Nothing, LexerError ("Unexpected character '" ++ [c] ++ "' at position " ++ show i))
 
